6a518cf966c92d9ab3ceafc6d1f4605e3bb60eab
DaneshAwati1993 <danesh.awati@gmail.com>
2025-11-07 16:46:20 +0530

    NetSuite Preparation

---
+++ Bimbo/src/test/java/NetSuite/NetSuiteTimeSheet.java
@@ -0,0 +1,115 @@
+package NetSuite;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NetSuiteTimeSheet {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriver driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.manage().window().maximize();
+		driver.get("https://system.netsuite.com/pages/customerlogin.jsp?country=US");
+		
+		//Login : 
+		
+		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("danesh.a@ivymobility.com");
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("Shilpa@9019");
+		
+		WebElement RememberMe = driver.findElement(By.xpath("//input[@id='rememberme']"));
+		
+		// Check if checkbox is enabled
+        if (RememberMe.isEnabled()) {
+            System.out.println("Checkbox is enabled.");
+
+            // If not already selected, click it to enable
+            if (!RememberMe.isSelected()) {
+            	RememberMe.click();
+                System.out.println("Checkbox is now selected (enabled).");
+            } else {
+                System.out.println("Checkbox is already selected.");
+            }
+        } else {
+            System.out.println("Checkbox is disabled — cannot be clicked.");
+        }
+
+        // Optional: verify after clicking
+        if (RememberMe.isSelected()) {
+            System.out.println("✅ Remember Me checkbox is enabled & selected successfully!");
+        } else {
+            System.out.println("❌ Checkbox could not be selected.");
+        }
+		
+        
+        //Click Login : 
+		driver.findElement(By.xpath("//button[@id='login-submit']")).click();
+		
+		 try {
+	            Thread.sleep(2000); // use WebDriverWait in real tests
+	        } catch (InterruptedException e) {
+	            e.printStackTrace();
+	        }
+		 
+		 
+		// Fetch the page title
+	        String pageTitle1 = driver.getTitle();
+	        System.out.println("Current Page Title: " + pageTitle1);
+
+	        // Check if the "Additional Authentication Required" page is displayed
+	        if (pageTitle1.equalsIgnoreCase("Additional Authentication Required")) {
+	        	System.out.println("✅ Authentication page detected — proceeding with entry steps...");
+	        	
+	            // Continue steps for this page
+	            driver.findElement(By.xpath("//*[@id=\"null\"]")).sendKeys("danu");  //
+	            driver.findElement(By.xpath("/html/body/div[2]/div[1]/form/table/tbody/tr[4]/td/input")).click();
+	        } else {
+	            System.out.println("ℹ️ Different page detected — skipping authentication steps.");
+	        }
+	        
+	        Thread.sleep(3000);
+	        
+	        
+			// Fetch the page title
+	        String pageTitle2 = driver.getTitle();
+	        System.out.println("Current Page Title: " + pageTitle2);
+
+	        // Check if the "Additional Authentication Required" page is displayed
+	        if (pageTitle2.equalsIgnoreCase("Home - NetSuite (Ivy Mobile Technologies)")) {
+	        	System.out.println("Home - NetSuite (Ivy Mobile Technologies -- Is detected Proceeding for Timesheet Menu");
+	        	
+	            // Proceeding for Timesheet Page
+	            driver.findElement(By.xpath("//a[normalize-space(text())='Weekly Timesheet']")).click();
+
+	        } else {
+	            System.out.println("ℹ️ Different page detected — skipping authentication steps.");
+	        }
+
+	        //Validate Timesheet menu available or not : 
+	        
+	        String pageTitle3 = driver.getTitle();
+	        System.out.println("Current Page Title: " + pageTitle3);
+
+	        if (pageTitle3.equalsIgnoreCase("Weekly Timesheet - NetSuite (Ivy Mobile Technologies)")) {
+	        	System.out.println("Home - NetSuite (Ivy Mobile Technologies -- Is detected Proceeding for Timesheet Menu");
+	        	
+	            // Proceeding for Timesheet Page
+	            //driver.findElement(By.xpath("//a[@id=\"ns-component-13384525273380528--52605341\"]")).click();
+
+	        } else {
+	            System.out.println("ℹ️ Different page detected — skipping authentication steps.");
+	        }
+	        
+	        
+	        
+	        
+	        
+		
+		
+	}
+
+}
